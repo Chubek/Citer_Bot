3d83cfa7c72ca725cb1bc10826ea6519a2c47900
Chubek <aurlito@riseup.net>
2017-11-30 17:33:56 +0330

    nn

--- scraper.py
+++ scraper.py
@@ -1,5 +1,24 @@
 #scrape the quote
 
 from bs4 import BeautifulSoup
-import urllib3
+import requests
+import random
 
+
+def random_page():
+    page_num = random.randint(2, 2880)
+
+    return "http://citation-celebre.leparisien.fr/liste-citation?nationalite=france&page=" + str(page_num)
+
+
+def get_quote():
+    url = random_page()
+    r = requests.get(url)
+
+    soup = BeautifulSoup(r.content, "html.parser")
+
+    quotes = soup.findAll('q')
+
+    return random.choice(quotes)
+
+print(get_quote())
